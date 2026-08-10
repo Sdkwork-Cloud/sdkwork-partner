a0870f07a6ba060f74797b2ef1fa7a7474a8c0f5
zowork <sdkwork@zowalk.com>
2026-08-10 19:07:11 +0800

    chore: sync local changes

--- database/migrations/postgres/0001_organization_id_not_null.up.sql
+++ database/migrations/postgres/0001_organization_id_not_null.up.sql
@@ -18,46 +18,57 @@
 
 BEGIN;
 
+ALTER TABLE partner_level ADD COLUMN IF NOT EXISTS organization_id BIGINT NOT NULL DEFAULT 0;
 UPDATE partner_level SET organization_id = 0 WHERE organization_id IS NULL;
 ALTER TABLE partner_level ALTER COLUMN organization_id SET DEFAULT 0;
 ALTER TABLE partner_level ALTER COLUMN organization_id SET NOT NULL;
 
+ALTER TABLE partner_commission_config ADD COLUMN IF NOT EXISTS organization_id BIGINT NOT NULL DEFAULT 0;
 UPDATE partner_commission_config SET organization_id = 0 WHERE organization_id IS NULL;
 ALTER TABLE partner_commission_config ALTER COLUMN organization_id SET DEFAULT 0;
 ALTER TABLE partner_commission_config ALTER COLUMN organization_id SET NOT NULL;
 
+ALTER TABLE partner_partner ADD COLUMN IF NOT EXISTS organization_id BIGINT NOT NULL DEFAULT 0;
 UPDATE partner_partner SET organization_id = 0 WHERE organization_id IS NULL;
 ALTER TABLE partner_partner ALTER COLUMN organization_id SET DEFAULT 0;
 ALTER TABLE partner_partner ALTER COLUMN organization_id SET NOT NULL;
 
+ALTER TABLE partner_customer_binding ADD COLUMN IF NOT EXISTS organization_id BIGINT NOT NULL DEFAULT 0;
 UPDATE partner_customer_binding SET organization_id = 0 WHERE organization_id IS NULL;
 ALTER TABLE partner_customer_binding ALTER COLUMN organization_id SET DEFAULT 0;
 ALTER TABLE partner_customer_binding ALTER COLUMN organization_id SET NOT NULL;
 
+ALTER TABLE partner_join_fee_payment ADD COLUMN IF NOT EXISTS organization_id BIGINT NOT NULL DEFAULT 0;
 UPDATE partner_join_fee_payment SET organization_id = 0 WHERE organization_id IS NULL;
 ALTER TABLE partner_join_fee_payment ALTER COLUMN organization_id SET DEFAULT 0;
 ALTER TABLE partner_join_fee_payment ALTER COLUMN organization_id SET NOT NULL;
 
+ALTER TABLE partner_commission_event ADD COLUMN IF NOT EXISTS organization_id BIGINT NOT NULL DEFAULT 0;
 UPDATE partner_commission_event SET organization_id = 0 WHERE organization_id IS NULL;
 ALTER TABLE partner_commission_event ALTER COLUMN organization_id SET DEFAULT 0;
 ALTER TABLE partner_commission_event ALTER COLUMN organization_id SET NOT NULL;
 
+ALTER TABLE partner_commission_settlement ADD COLUMN IF NOT EXISTS organization_id BIGINT NOT NULL DEFAULT 0;
 UPDATE partner_commission_settlement SET organization_id = 0 WHERE organization_id IS NULL;
 ALTER TABLE partner_commission_settlement ALTER COLUMN organization_id SET DEFAULT 0;
 ALTER TABLE partner_commission_settlement ALTER COLUMN organization_id SET NOT NULL;
 
+ALTER TABLE partner_commission_distribution ADD COLUMN IF NOT EXISTS organization_id BIGINT NOT NULL DEFAULT 0;
 UPDATE partner_commission_distribution SET organization_id = 0 WHERE organization_id IS NULL;
 ALTER TABLE partner_commission_distribution ALTER COLUMN organization_id SET DEFAULT 0;
 ALTER TABLE partner_commission_distribution ALTER COLUMN organization_id SET NOT NULL;
 
+ALTER TABLE partner_withdrawal ADD COLUMN IF NOT EXISTS organization_id BIGINT NOT NULL DEFAULT 0;
 UPDATE partner_withdrawal SET organization_id = 0 WHERE organization_id IS NULL;
 ALTER TABLE partner_withdrawal ALTER COLUMN organization_id SET DEFAULT 0;
 ALTER TABLE partner_withdrawal ALTER COLUMN organization_id SET NOT NULL;
 
+ALTER TABLE partner_stat_snapshot ADD COLUMN IF NOT EXISTS organization_id BIGINT NOT NULL DEFAULT 0;
 UPDATE partner_stat_snapshot SET organization_id = 0 WHERE organization_id IS NULL;
 ALTER TABLE partner_stat_snapshot ALTER COLUMN organization_id SET DEFAULT 0;
 ALTER TABLE partner_stat_snapshot ALTER COLUMN organization_id SET NOT NULL;
 
+ALTER TABLE partner_audit_log ADD COLUMN IF NOT EXISTS organization_id BIGINT NOT NULL DEFAULT 0;
 UPDATE partner_audit_log SET organization_id = 0 WHERE organization_id IS NULL;
 ALTER TABLE partner_audit_log ALTER COLUMN organization_id SET DEFAULT 0;
 ALTER TABLE partner_audit_log ALTER COLUMN organization_id SET NOT NULL;
